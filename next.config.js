33f14e263802acabb61c71c37d46c559905ea780
Camilo Useche <camilouseche22@gmail.com>
2025-09-21 17:16:10 +0000

    2025-09-21T17:15:40Z [web] <Firebase Studio> Starting preview... 2025-09

---
+++ next.config.js
@@ -0,0 +1,49 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.nationalgeographicla.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  allowedDevOrigins: [
+      "https://6000-firebase-studio-1757878043109.cluster-r7kbxfo3fnev2vskbkhhphetq6.cloudworkstations.dev"
+  ]
+};
+
+module.exports = nextConfig;
